289a05b089b784d508e3034541caf8b5872df6b0
Lukasz <edzio94@gmail.com>
2016-05-15 15:06:48 +0200

    Creating SignalChecker

--- checkUniqueSignal.m
+++ checkUniqueSignal.m
@@ -1,8 +1,15 @@
-function [ uniqueSignal ] = checkUniqueSignal( Array,i,mySignal )
+function [ checkSignal ] = checkUniqueSignal( i,Signal,onSignal )
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
-uniqueSignal = false;
+A = [1 0 0 0 1];
+checkSignal = onSignal;
+indexes = strfind(Signal,A);
 
+for index=1:indexes
+    if (i == index) and (checkSignal == false)
+        checkSignal= true;
+    end
+end
 
 end
 
